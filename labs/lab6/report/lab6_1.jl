41b5caac374815dddb248d501dbbf8c9766d673f
AbdullinaLR <owlwithcookie@gmail.com>
2024-03-12 02:05:50 +0300

    lab6 rutube is down

---
+++ labs/lab6/report/lab6_1.jl
@@ -0,0 +1,33 @@
+using DifferentialEquations
+using Plots
+
+N = 12800
+I0 = 180
+R0 = 58
+S0 = N - I0 - R0
+a = 0.4
+b = 0.32
+
+function ode_fn(du, u, p, t)
+	S, I, R = u
+	du[1] = 0
+	du[2] = -b*u[2]
+	du[3] = b*I
+	
+end
+
+v0 = [S0, I0, R0]
+tspan = (0.0, 60.0)
+prob = ODEProblem(ode_fn, v0, tspan)
+sol = solve(prob, dtmax=0.05)
+S = [u[1] for u in sol.u]
+I = [u[2] for u in sol.u]
+R = [u[3] for u in sol.u]
+T = [t for t in sol.t]
+
+plt = plot(dpi=600, legend = :topright)
+plot!(plt, T, S, label = "Восприимчивые особи" ,color=:green)
+plot!(plt, T, I, label = "Инфицированные особи" ,color=:red)
+plot!(plt, T, R, label = "Особи с иммунитетом" ,color=:blue)
+savefig(plt,"lab6_1_jl.png")
+
